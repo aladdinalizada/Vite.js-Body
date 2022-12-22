68a0c5fed62a461692596b2239aa9c2aa5efe7a8
aladdinalizada <tu7c5zneb@code.edu.az>
2022-12-22 16:52:21 +0400

    Vita.js body

---
+++ test-project/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Practice from "./assets/components/practice-1";
+
+function App() {
+  return (
+    <div className="App">
+      <Practice />
+    </div>
+  );
+}
+
+export default App;
